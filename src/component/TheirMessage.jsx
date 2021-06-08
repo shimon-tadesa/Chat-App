bd3a0df1ea53f6b5bc0a28dd448f3c1d0cf56ec6
shimon tadesa <shimontadesa123@gmail.com>
2021-06-08 23:04:10 +0300

    add form and input and functions for user messages

--- src/component/TheirMessage.jsx
+++ src/component/TheirMessage.jsx
@@ -8,6 +8,25 @@ const TheirMessage = ({ lastMessage, message }) => {
           style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
         />
       )}
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            backgroundColor: "#CABCDC",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
     </div>
   );
 };
